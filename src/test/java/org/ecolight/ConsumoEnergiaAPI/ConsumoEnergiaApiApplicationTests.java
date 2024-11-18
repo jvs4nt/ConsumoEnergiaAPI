1a112aa78ac5775e647d276714ae74ed2bbf0c78
jvs4nt <joaovit342@gmail.com>
2024-11-18 08:12:24 -0300

    inicializando projeto

---
+++ src/test/java/org/ecolight/ConsumoEnergiaAPI/ConsumoEnergiaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ecolight.ConsumoEnergiaAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumoEnergiaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
