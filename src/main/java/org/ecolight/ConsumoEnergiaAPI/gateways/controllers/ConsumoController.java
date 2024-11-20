df2faa7e83d764b34fd60b66eab4d3bfcdc4ced6
jvs4nt <joao.santana@noctech.io>
2024-11-20 16:47:38 -0300

    implementando swagger

--- src/main/java/org/ecolight/ConsumoEnergiaAPI/gateways/controllers/ConsumoController.java
+++ src/main/java/org/ecolight/ConsumoEnergiaAPI/gateways/controllers/ConsumoController.java
@@ -1,5 +1,7 @@
 package org.ecolight.ConsumoEnergiaAPI.gateways.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.ecolight.ConsumoEnergiaAPI.domains.Consumo;
 import org.ecolight.ConsumoEnergiaAPI.usecases.ConsumoService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,18 +13,21 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/consumos")
+@Tag(name = "Consumos", description = "Gerenciamento de consumos de energia")
 public class ConsumoController {
 
     @Autowired
     private ConsumoService consumoService;
 
     @PostMapping
+    @Operation(summary = "Criar um novo consumo", description = "Adiciona um novo consumo ao sistema")
     public ResponseEntity<Consumo> criarConsumo(@RequestBody Consumo consumo) {
         Consumo novoConsumo = consumoService.salvarConsumo(consumo);
         return ResponseEntity.status(HttpStatus.CREATED).body(novoConsumo);
     }
 
     @GetMapping
+    @Operation(summary = "Listar todos os consumos", description = "Retorna uma lista com todos os consumos registrados")
     public ResponseEntity<List<Consumo>> listarConsumos() {
         List<Consumo> consumos = consumoService.listarTodosConsumos();
         return ResponseEntity.ok(consumos);
