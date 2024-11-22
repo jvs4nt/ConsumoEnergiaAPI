d9fa7dda6ebf805fcc44d46e9f9ea700059097e4
jvs4nt <joao.santana@noctech.io>
2024-11-22 02:55:30 -0300

    swagger e hateoas metas

--- src/main/java/org/ecolight/ConsumoEnergiaAPI/gateways/controllers/MetaController.java
+++ src/main/java/org/ecolight/ConsumoEnergiaAPI/gateways/controllers/MetaController.java
@@ -1,45 +1,86 @@
 package org.ecolight.ConsumoEnergiaAPI.gateways.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.ecolight.ConsumoEnergiaAPI.domains.Meta;
 import org.ecolight.ConsumoEnergiaAPI.usecases.MetaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/metas")
+@Tag(name = "Metas", description = "Gerenciamento de metas do sistema")
 public class MetaController {
 
     @Autowired
     private MetaService metaService;
 
     @PostMapping
-    public ResponseEntity<Meta> criarMeta(@RequestBody Meta meta) {
+    @Operation(summary = "Criar uma nova meta", description = "Adiciona uma nova meta ao sistema")
+    public ResponseEntity<EntityModel<Meta>> criarMeta(@RequestBody Meta meta) {
         Meta novaMeta = metaService.salvarMeta(meta);
-        return ResponseEntity.status(201).body(novaMeta);
+        EntityModel<Meta> resource = EntityModel.of(novaMeta);
+        resource.add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(MetaController.class)
+                .buscarMetaPorId(novaMeta.getId())).withSelfRel());
+        resource.add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(MetaController.class)
+                .listarMetas()).withRel("all-metas"));
+        return ResponseEntity.created(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(MetaController.class)
+                .buscarMetaPorId(novaMeta.getId())).toUri()).body(resource);
     }
 
     @GetMapping
-    public ResponseEntity<List<Meta>> listarMetas() {
-        return ResponseEntity.ok(metaService.listarTodasMetas());
+    @Operation(summary = "Listar todas as metas", description = "Retorna uma lista de todas as metas registradas")
+    public ResponseEntity<List<EntityModel<Meta>>> listarMetas() {
+        List<EntityModel<Meta>> metas = metaService.listarTodasMetas().stream()
+                .map(meta -> {
+                    EntityModel<Meta> resource = EntityModel.of(meta);
+                    resource.add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(MetaController.class)
+                            .buscarMetaPorId(meta.getId())).withSelfRel());
+                    return resource;
+                })
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(metas);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Meta> buscarMetaPorId(@PathVariable Integer id) {
-        Optional<Meta> meta = metaService.buscarPorId(id);
-        return meta.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    @Operation(summary = "Buscar meta por ID", description = "Retorna uma meta específica pelo ID fornecido")
+    public ResponseEntity<EntityModel<Meta>> buscarMetaPorId(@PathVariable Integer id) {
+        return metaService.buscarPorId(id)
+                .map(meta -> {
+                    EntityModel<Meta> resource = EntityModel.of(meta);
+                    resource.add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(MetaController.class)
+                            .buscarMetaPorId(id)).withSelfRel());
+                    resource.add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(MetaController.class)
+                            .listarMetas()).withRel("all-metas"));
+                    return ResponseEntity.ok(resource);
+                })
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Meta> atualizarMeta(@PathVariable Integer id, @RequestBody Meta meta) {
-        Optional<Meta> metaAtualizada = metaService.atualizarMeta(id, meta);
-        return metaAtualizada.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    @Operation(summary = "Atualizar uma meta", description = "Atualiza os dados de uma meta existente")
+    public ResponseEntity<EntityModel<Meta>> atualizarMeta(@PathVariable Integer id, @RequestBody Meta meta) {
+        return metaService.atualizarMeta(id, meta)
+                .map(metaAtualizada -> {
+                    EntityModel<Meta> resource = EntityModel.of(metaAtualizada);
+                    resource.add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(MetaController.class)
+                            .buscarMetaPorId(metaAtualizada.getId())).withSelfRel());
+                    resource.add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(MetaController.class)
+                            .listarMetas()).withRel("all-metas"));
+                    return ResponseEntity.ok(resource);
+                })
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Excluir uma meta", description = "Remove uma meta existente pelo ID fornecido")
     public ResponseEntity<Void> excluirMeta(@PathVariable Integer id) {
         if (metaService.excluirMeta(id)) {
             return ResponseEntity.noContent().build();
